80f1a7cd8f6868cc86a02d63c533389b81804fb5
Экизян Владимир <9885313888va@gmail.com>
2026-02-28 15:18:38 +0300

    Lab2 Task4.java

---
+++ Lab2/Task4.java
@@ -0,0 +1,11 @@
+package Lab2;
+import java.util.Scanner;
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число (1-12):");
+        int month = scanner.nextInt();
+        scanner.close();
+    }
+}
