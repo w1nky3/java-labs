0dfd883de561dac22e4d8180b496ca32d6a1d45a
Экизян Владимир <9885313888va@gmail.com>
2026-03-07 02:52:48 +0300

    Fix input prompt and improve month validation

--- Lab2/Task4.java
+++ Lab2/Task4.java
@@ -1,11 +1,40 @@
-package Lab2;
 import java.util.Scanner;
+
 public class Task4 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите число (1-12):");
+        // Ввод номера месяца
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите номер месяца (1-12): ");
         int month = scanner.nextInt();
         scanner.close();
+
+        // Определение количества дней в месяце
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("31 день");
+                break;
+
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println("30 дней");
+                break;
+
+            case 2:
+                System.out.println("28 или 29 дней");
+                break;
+
+            default:
+                System.out.println("Ошибка: некорректный номер месяца");
+        }
+
     }
 }
